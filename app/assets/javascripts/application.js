bfff6db06b07b60b1c2cc193eddab827ce089e96
Alexander Adam <mail@alexander-adam.info>
2016-05-22 20:39:51 +0200

    add reactrb support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+Opal.load('components');
 //= require bootstrap-sprockets
 //= require bootstrap-material-design
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :notes
+  mount ReactiveRecord::Engine => "/rr"
 
   root 'notes#index'
 end
