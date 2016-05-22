e4a1c4673e85adfed4e79c2aed8db5fdc071da24
Alexander Adam <mail@alexander-adam.info>
2016-05-22 20:11:09 +0200

    enable configuation via (Docker) env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.consider_all_requests_local = true
+  if ENV['TRUSTED_IP'].present?
+    BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if defined?(BetterErrors)
+  end
 end
