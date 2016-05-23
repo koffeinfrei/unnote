b4fc2fdb23e3c306af140cf3a92d7777d0bf5d1e
Alexis Reigel <mail@koffeinfrei.org>
2016-05-23 13:10:56 +0200

    Revert "Merge pull request #2 from panter/features/add_reactrb_support"
    This reverts commit 57030a4e957fe20209f45dd52784bb80bd6b7a00, reversing
    changes made to 60cf598b82b00c3f3edb6c3cd796a3ad7fefd1fc.
    
    Conflicts:
    	Gemfile
    	Gemfile.lock

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-Opal.load('components');
 //= require bootstrap-sprockets
 //= require bootstrap-material-design
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :notes
-  mount ReactiveRecord::Engine => "/rr"
 
   root 'notes#index'
 end
