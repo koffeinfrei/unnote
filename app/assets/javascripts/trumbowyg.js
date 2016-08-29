5a8504bf5b3194c41464fa7b3c6840a583f94532
Alexis Reigel <mail@koffeinfrei.org>
2016-08-29 21:11:11 +0200

    update trumbowyg (incl. textarea hack reapplied)

--- app/assets/javascripts/trumbowyg.js
+++ app/assets/javascripts/trumbowyg.js
@@ -1,5 +1,5 @@
 /**
- * Trumbowyg v2.1.0 - A lightweight WYSIWYG editor
+ * Trumbowyg v2.1.6hack - A lightweight WYSIWYG editor
  * Trumbowyg core file
  *
  * this version is a quick hack that removes the creation of a textarea,
@@ -12,6 +12,7 @@
  * @author Alexandre Demode (Alex-D)
  *         Twitter : @AlexandreDemode
  *         Website : alex-d.fr
+ * @author Alexis Reigel
  */
 
 jQuery.trumbowyg = {
@@ -152,7 +153,6 @@ jQuery.trumbowyg = {
         t.doc = editorElem.ownerDocument || document;
 
         // jQuery object of the editor
-        t.$ta = $(editorElem); // $ta : Textarea
         t.$c = $(editorElem); // $c : creator
 
         options = options || {};
@@ -167,7 +167,7 @@ jQuery.trumbowyg = {
         // SVG path
         var svgPathOption = $.trumbowyg.svgPath != null ? $.trumbowyg.svgPath : options.svgPath;
         t.hasSvg = svgPathOption !== false;
-        t.svgPath = !!t.doc.querySelector('base') ? window.location : '';
+        t.svgPath = !!t.doc.querySelector('base') ? window.location.href.split('#')[0] : '';
         if ($('#' + trumbowygIconsId, t.doc).length === 0 && svgPathOption !== false) {
             if (svgPathOption == null) {
                 try {
@@ -179,7 +179,7 @@ jQuery.trumbowyg = {
                         if (!stackLines[i].match(/http[s]?:\/\//)) {
                             continue;
                         }
-                        svgPathOption = stackLines[Number(i)].match(/((http[s]?:\/\/.+\/)([^\/]+\.js)):/)[1].split('/');
+                        svgPathOption = stackLines[Number(i)].match(/((http[s]?:\/\/.+\/)([^\/]+\.js))(\?.*)?:/)[1].split('/');
                         svgPathOption.pop();
                         svgPathOption = svgPathOption.join('/') + '/ui/icons.svg';
                         break;
@@ -190,8 +190,18 @@ jQuery.trumbowyg = {
             var div = t.doc.createElement('div');
             div.id = trumbowygIconsId;
             t.doc.body.insertBefore(div, t.doc.body.childNodes[0]);
-            $.get(svgPathOption, function (data) {
-                div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+            $.ajax({
+                async: true,
+                type: 'GET',
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                dataType: 'xml',
+                url: svgPathOption,
+                data: null,
+                beforeSend: null,
+                complete: null,
+                success: function (data) {
+                    div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
+                }
             });
         }
 
@@ -444,11 +454,13 @@ jQuery.trumbowyg = {
     Trumbowyg.prototype = {
         init: function () {
             var t = this;
-
             t.initPlugins();
 
-            // Disable image resize in Firefox
-            t.doc.execCommand('enableObjectResizing', false, false);
+            try {
+                // Disable image resize, try-catch for old IE
+                t.doc.execCommand('enableObjectResizing', false, false);
+            } catch (e) {
+            }
             t.doc.execCommand('defaultParagraphSeparator', false, 'p');
 
             t.buildEditor();
@@ -479,24 +491,14 @@ jQuery.trumbowyg = {
                 class: prefix + 'box ' + prefix + 'editor-visible ' + prefix + t.o.lang + ' trumbowyg'
             });
 
-            // $ta = Textarea
             // $ed = Editor
-            t.isTextarea = t.$ta.is('textarea');
-            if (t.isTextarea) {
-                html = t.$ta.val();
-                t.$ed = $('<div/>');
-                t.$box
-                    .insertAfter(t.$ta)
-                    .append(t.$ed, t.$ta);
-            } else {
-                t.$ed = t.$ta;
-                html = t.$ed.html();
+            t.$ed = t.$c;
+            html = t.$ed.html();
 
-                t.$box
-                    .insertAfter(t.$ed)
-                    .append(t.$ed);
-                t.syncCode();
-            }
+            t.$box
+                .insertAfter(t.$ed)
+                .append(t.$ed);
+            t.syncCode();
 
             t.$ed
                 .addClass(prefix + 'editor')
@@ -522,14 +524,17 @@ jQuery.trumbowyg = {
             t.semanticCode();
 
 
-            t._ctrl = false;
+            var ctrl = false,
+                composition = false,
+                debounceButtonPaneStatus;
+
             t.$ed
                 .on('dblclick', 'img', t.o.imgDblClickHandler)
                 .on('keydown', function (e) {
-                    t._composition = (e.which === 229);
+                    composition = (e.which === 229);
 
                     if (e.ctrlKey) {
-                        t._ctrl = true;
+                        ctrl = true;
                         var k = t.keys[String.fromCharCode(e.which).toUpperCase()];
 
                         try {
@@ -539,24 +544,27 @@ jQuery.trumbowyg = {
                         }
                     }
                 })
-                .on('keyup', function (e) {
+                .on('keyup input', function (e) {
                     if (e.which >= 37 && e.which <= 40) {
                         return;
                     }
 
                     if (e.ctrlKey && (e.which === 89 || e.which === 90)) {
                         t.$c.trigger('tbwchange');
-                    } else if (!t._ctrl && e.which !== 17 && !t._composition) {
+                    } else if (!ctrl && e.which !== 17 && !composition) {
                         t.semanticCode(false, e.which === 13);
                         t.$c.trigger('tbwchange');
                     }
 
                     setTimeout(function () {
-                        t._ctrl = false;
+                        ctrl = false;
                     }, 200);
                 })
                 .on('mouseup keydown keyup', function () {
-                    t.updateButtonPaneStatus();
+                    clearTimeout(debounceButtonPaneStatus);
+                    debounceButtonPaneStatus = setTimeout(function () {
+                        t.updateButtonPaneStatus();
+                    }, 50);
                 })
                 .on('focus blur', function (e) {
                     t.$c.trigger('tbw' + e.type);
@@ -565,7 +573,10 @@ jQuery.trumbowyg = {
                     }
                 })
                 .on('cut', function () {
-                    t.$c.trigger('tbwchange');
+                    setTimeout(function () {
+                        t.semanticCode(false, true);
+                        t.$c.trigger('tbwchange');
+                    }, 0);
                 })
                 .on('paste', function (e) {
                     if (t.o.removeformatPasted) {
@@ -594,17 +605,13 @@ jQuery.trumbowyg = {
                     });
 
                     setTimeout(function () {
-                        if (t.o.semantic) {
-                            t.semanticCode(false, true);
-                        } else {
-                            t.syncCode();
-                        }
+                        t.semanticCode(false, true);
                         t.$c.trigger('tbwpaste', e);
                     }, 0);
                 });
 
-            $(t.doc).on('keydown', function (e) {
-                if (e.which === 27) {
+            t.$box.on('keydown', function (e) {
+                if (e.which === 27 && $('.' + prefix + 'modal-box', t.$box).length === 1) {
                     t.closeModal();
                     return false;
                 }
@@ -849,24 +856,14 @@ jQuery.trumbowyg = {
                 prefix = t.o.prefix,
                 height = t.height;
 
-            if (t.isTextarea) {
-                t.$box.after(
-                    t.$ta
-                        .css({height: height})
-                        .val(t.html())
-                        .removeClass(prefix + 'textarea')
-                        .show()
-                );
-            } else {
-                t.$box.after(
-                    t.$ed
-                        .css({height: height})
-                        .removeClass(prefix + 'editor')
-                        .removeAttr('contenteditable')
-                        .html(t.html())
-                        .show()
-                );
-            }
+            t.$box.after(
+                t.$ed
+                    .css({height: height})
+                    .removeClass(prefix + 'editor')
+                    .removeAttr('contenteditable')
+                    .html(t.html())
+                    .show()
+            );
 
             t.$ed.off('dblclick', 'img');
 
@@ -875,6 +872,7 @@ jQuery.trumbowyg = {
             t.$box.remove();
             t.$c.removeData('trumbowyg');
             $('body').removeClass(prefix + 'body-fullscreen');
+            t.$c.trigger('tbwclose');
         },
 
 
@@ -939,8 +937,6 @@ jQuery.trumbowyg = {
             return '';
         },
         syncTextarea: function () {
-            var t = this;
-            // t.$ed.find('hr,img,embed,input').length > 0 ? t.$ed.html() : '';
         },
         syncCode: function (force) {
             var t = this;
@@ -962,7 +958,6 @@ jQuery.trumbowyg = {
             if (t.o.semantic) {
                 t.semanticTag('b', 'strong');
                 t.semanticTag('i', 'em');
-                t.semanticTag('strike', 'del');
 
                 if (full) {
                     var inlineElementsSelector = t.o.inlineElementsSelector,
@@ -976,7 +971,7 @@ jQuery.trumbowyg = {
                     // Wrap groups of inline elements in paragraphs (recursive)
                     var wrapInlinesInParagraphsFrom = function ($from) {
                         if ($from.length !== 0) {
-                            var $finalParagraph = $from.nextUntil(blockElementsSelector).andSelf().wrapAll('<p/>').parent(),
+                            var $finalParagraph = $from.nextUntil(blockElementsSelector).addBack().wrapAll('<p/>').parent(),
                                 $nextElement = $finalParagraph.nextAll(inlineElementsSelector).first();
                             $finalParagraph.next('br').remove();
                             wrapInlinesInParagraphsFrom($nextElement);
@@ -1140,7 +1135,10 @@ jQuery.trumbowyg = {
                 t.$ed.focus();
             }
 
-            t.doc.execCommand('styleWithCSS', false, forceCss || false);
+            try {
+                t.doc.execCommand('styleWithCSS', false, forceCss || false);
+            } catch (c) {
+            }
 
             try {
                 t[cmd + skipTrumbowyg](param);
@@ -1270,12 +1268,12 @@ jQuery.trumbowyg = {
             t.$overlay.off();
 
             // Find the modal box
-            var $mb = $('.' + prefix + 'modal-box', t.$box);
+            var $modalBox = $('.' + prefix + 'modal-box', t.$box);
 
-            $mb.animate({
-                top: '-' + $mb.height()
+            $modalBox.animate({
+                top: '-' + $modalBox.height()
             }, 100, function () {
-                $mb.parent().remove();
+                $modalBox.parent().remove();
                 t.hideOverlay();
             });
 
@@ -1430,7 +1428,7 @@ jQuery.trumbowyg = {
         updateButtonPaneStatus: function () {
             var t = this,
                 prefix = t.o.prefix,
-                tags = t.getTagsRecursive(t.doc.getSelection().focusNode.parentNode),
+                tags = t.getTagsRecursive(t.doc.getSelection().focusNode),
                 activeClasses = prefix + 'active-button ' + prefix + 'active';
 
             $('.' + prefix + 'active-button', t.$btnPane).removeClass(activeClasses);
@@ -1454,6 +1452,12 @@ jQuery.trumbowyg = {
             var t = this;
             tags = tags || [];
 
+            if (element) {
+                element = element.parentNode;
+            } else {
+                return tags;
+            }
+
             var tag = element.tagName;
             if (tag === 'DIV') {
                 return tags;
@@ -1468,7 +1472,7 @@ jQuery.trumbowyg = {
 
             tags.push(tag);
 
-            return t.getTagsRecursive(element.parentNode, tags);
+            return t.getTagsRecursive(element, tags);
         },
 
         // Plugins
