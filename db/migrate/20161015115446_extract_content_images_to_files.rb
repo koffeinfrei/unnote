26136e7b527106f7a366ed8738d87e3fd68ee06f
Alexis Reigel <mail@koffeinfrei.org>
2016-10-15 13:56:27 +0200

    data migration: extract content images to files

---
+++ db/migrate/20161015115446_extract_content_images_to_files.rb
@@ -0,0 +1,13 @@
+class ExtractContentImagesToFiles < ActiveRecord::Migration
+  def change
+    # don't update updated_at
+    ActiveRecord::Base.record_timestamps = false
+    begin
+      Note.find_each do |note|
+        note.update_attributes!(content: note.content)
+      end
+    ensure
+      ActiveRecord::Base.record_timestamps = true
+    end
+  end
+end
