3541c4206cdbb1ba63653281d3925b3c78dab227
Alexis Reigel <mail@koffeinfrei.org>
2016-10-15 13:25:04 +0200

    extract methods

--- app/models/concerns/has_content.rb
+++ app/models/concerns/has_content.rb
@@ -2,27 +2,10 @@ module HasContent
   extend ActiveSupport::Concern
 
   def content=(content)
-    matches = content.scan(/<img.*? src=['"](data:image\/[^;]+;base64,[^'"]+)['"].*?>/).flatten
+    new_image_files = extract_image_files_from_content(content)
 
-    files = matches.map do |match|
-      Base64StringIO.new(match).tap do |file|
-        content.sub!(match, file.file_name)
-      end
-    end
-
-    new_file_names = files.map(&:original_filename)
-
-    # remove obsolete images
-    images.select { |image| !new_file_names.include?(image.file.original_filename) }.each do |image|
-      self.images.delete(image)
-      image.remove!
-    end
-
-    # add new ones
-    old_file_names = images.map(&:file).map(&:original_filename)
-    files.select { |image| !old_file_names.include?(image.original_filename) }.each do |image|
-      self.images += [image]
-    end
+    remove_obsolete_files!(new_image_files)
+    add_new_files!(new_image_files)
 
     # FIXME: https://github.com/carrierwaveuploader/carrierwave/issues/1990
     self[:images] = images.map(&:file).map(&:original_filename)
@@ -42,5 +25,40 @@ module HasContent
     end
 
     content
+  end
+
+  private
+
+  def extract_image_files_from_content(content)
+    matches = content
+      .scan(/<img.*? src=['"](data:image\/[^;]+;base64,[^'"]+)['"].*?>/)
+      .flatten
+
+    matches.map do |match|
+      Base64StringIO.new(match).tap do |file|
+        content.sub!(match, file.file_name)
+      end
+    end
+  end
+
+  def remove_obsolete_files!(new_image_files)
+    new_file_names = new_image_files.map(&:original_filename)
+
+    images
+      .select { |image| !new_file_names.include?(image.file.original_filename) }
+      .each do |image|
+        images.delete(image)
+        image.remove!
+      end
+  end
+
+  def add_new_files!(new_image_files)
+    old_file_names = images.map(&:file).map(&:original_filename)
+
+    new_image_files
+      .select { |image| !old_file_names.include?(image.original_filename) }
+      .each do |image|
+        self.images += [image]
+      end
   end
 end
