4dcdf9ece1cb13fba96ac17ba737c3828bc7c445
Alexis Reigel <mail@koffeinfrei.org>
2017-02-02 20:31:37 +0100

    fix note conflict spec

--- spec/features/edit_note_conflict_spec.rb
+++ spec/features/edit_note_conflict_spec.rb
@@ -7,8 +7,7 @@ RSpec.feature 'Edit note with conflict', :js do
     before { login_as user }
 
     scenario 'A copy of the note is created' do
-      date = Time.local(2016, 8, 1, 15, 33)
-      Timecop.freeze(date) do
+      Timecop.travel(Time.local(2016, 8, 1, 15, 33)) do
         note = Note.create!(
           title: 'my note',
           user: user,
@@ -16,32 +15,30 @@ RSpec.feature 'Edit note with conflict', :js do
           uid: SecureRandom.uuid
         )
 
+        # load the note...
         visit edit_note_path(note)
 
-        js_timecop_freeze(date) do
+        # ...in the meantime there's an edit
+        note.update_attributes! content: '<p>note content - update 1</p>'
 
-          # in the meantime there's an edit
-          note.update_attributes! content: 'note content - update 1'
+        find('.ql-editor').set('note content - update 2')
 
-          js_timecop_tick(1.second)
-
-          find('.trumbowyg-editor').set(' - update 2')
-
-          using_wait_time 6 do
-            expect(page).to have_content 'Synced'
-          end
-
-          expect(page).to have_content 'my note (conflict 2016-08-01 15:33)'
-
-          # the note has the updated content
-          expect(note.reload.content).to eq 'note content - update 2<br>'
-
-          # the conflict copy has the old content
-          expect(Note.last).to have_attributes(
-            title: 'my note (conflict 2016-08-01 15:33)',
-            content: 'note content - update 1'
-          )
+        # worst case is we have to wait 1 full autosave polling cycle
+        # (polling cycle is 5 seconds)
+        using_wait_time 6 do
+          expect(page).to have_content 'Saved'
         end
+
+        expect(page).to have_content 'my note (conflict 2016-08-01 15:33)'
+
+        # the note has the updated content
+        expect(note.reload.content).to eq '<p>note content - update 2</p>'
+
+        # the conflict copy has the old content
+        expect(Note.last).to have_attributes(
+          title: 'my note (conflict 2016-08-01 15:33)',
+          content: '<p>note content - update 1</p>'
+        )
       end
     end
   end
