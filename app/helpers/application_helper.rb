9b6358d80aaff9bed98a9b9eba09a401bed8f3ad
Alexis Reigel <mail@koffeinfrei.org>
2017-03-11 15:04:45 +0100

    new logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  extend self
+
+  def inline_svg(file_name)
+    file_path = Rails.root.join("app/assets/images/#{file_name}")
+
+    raise "The svg image '#{file_path}' does not exist!" unless File.exists?(file_path)
+
+    File.read(file_path).html_safe
+  end
 end
