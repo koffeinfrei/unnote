107d18c889d92ff657e1b3724612b9f6d4e41eae
Alexis Reigel <mail@koffeinfrei.org>
2017-07-27 08:52:59 +0200

    update ruby and gems

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,6 @@ module Mykonote
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     # Require `belongs_to` associations by default
     config.active_record.belongs_to_required_by_default = true
 
