f94e208092a3008ec21757f4b5e456528faa7a8a
Alexis Reigel <mail@koffeinfrei.org>
2018-11-23 21:18:56 +0100

    add model association user->notes
    this way we can destroy a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :registerable
+
+  has_many :notes, dependent: :destroy
 end
