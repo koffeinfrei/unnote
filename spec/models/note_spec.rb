9c651fbacaea8af0d2815f96d9fc8913417117b6
Alexis Reigel <mail@koffeinfrei.org>
2018-11-29 16:08:38 +0100

    extract helper methods / let for common code

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -3,13 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Note do
+  let(:image1_placeholder) do
+    '360593ff547c864bd9d16bbed6eb8860d9fad9a407aa74e066039db23b525338'
+  end
+
+  let(:image1_data) do
+    'data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q='
+  end
+
+  let(:image2_data) do
+    'data:image/png;base64,/8c/4AAQSkZJRgABAQEASABKdhH//2Q='
+  end
+
   describe '#content=' do
     context 'when content contains 1 image' do
       let(:note) do
         described_class.create!(
           uid: SecureRandom.uuid,
-          user: User.create!(email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf'),
-          content: 'content1 <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=" />'
+          user: create_user,
+          content: %(content1 <img src="#{image1_data}" />)
         )
       end
 
@@ -22,15 +34,15 @@ RSpec.describe Note do
 
       it 'replaces the base64 part with the file hash' do
         expect(note.text_content).to eq(
-          'content1 <img src="360593ff547c864bd9d16bbed6eb8860d9fad9a407aa74e066039db23b525338" />'
+          %(content1 <img src="#{image1_placeholder}" />)
         )
       end
 
       it 'adds one image and removes one' do
         note.update!(
           content:
-          'content1 <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=" />' \
-          'content2 <img src="data:image/png;base64,/8c/4AAQSkZJRgABAQEASABKdhH//2Q=" />'
+            %(content1 <img src="#{image1_data}" />) +
+            %(content2 <img src="#{image2_data}" />)
         )
 
         first_file = note.images[0].file
@@ -40,9 +52,8 @@ RSpec.describe Note do
         expect(first_file).to exist
         expect(second_file).to exist
 
-        note.update_attributes!(
-          content:
-          'content2 <img src="data:image/png;base64,/8c/4AAQSkZJRgABAQEASABKdhH//2Q=" />'
+        note.update!(
+          content: %(content2 <img src="#{image2_data}" />)
         )
 
         expect(note.images.count).to eq 1
@@ -54,10 +65,10 @@ RSpec.describe Note do
     it 'saves equal images as one file' do
       note = described_class.create!(
         uid: SecureRandom.uuid,
-        user: User.create!(email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf'),
+        user: create_user,
         content:
-        'content1 <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=" />' \
-        'content2 <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=" />'
+          %(content1 <img src="#{image1_data}" />) +
+          %(content2 <img src="#{image1_data}" />)
       )
 
       expect(note.images.count).to eq 1
@@ -69,16 +80,16 @@ RSpec.describe Note do
     it 'retrieves equal images from one file' do
       note = described_class.create!(
         uid: SecureRandom.uuid,
-        user: User.create!(email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf'),
+        user: create_user,
         content:
-        'content1 <img src="360593ff547c864bd9d16bbed6eb8860d9fad9a407aa74e066039db23b525338" />' \
-        'content2 <img src="360593ff547c864bd9d16bbed6eb8860d9fad9a407aa74e066039db23b525338" />',
-        images: [Base64StringIO.new("data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=")]
+          %(content1 <img src="#{image1_placeholder}" />) +
+          %(content2 <img src="#{image1_placeholder}" />),
+        images: [Base64StringIO.new(image1_data)]
       )
 
       expect(note.content).to eq(
-        'content1 <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=" />' \
-        'content2 <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=" />' \
+        %(content1 <img src="#{image1_data}" />) +
+        %(content2 <img src="#{image1_data}" />)
       )
     end
   end
@@ -157,10 +168,10 @@ RSpec.describe Note do
     it 'duplicates the images' do
       note = described_class.create!(
         uid: SecureRandom.uuid,
-        user: User.create!(email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf'),
+        user: create_user,
         content:
-        'content1 <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQEASABKdhH//2Q=" />' \
-        'content2 <img src="data:image/png;base64,/9j/5AAQSkZJRgABAQEASABKdhH//2Q=" />',
+          %(content1 <img src="#{image1_data}" />) +
+          %(content2 <img src="#{image2_data}" />)
       )
 
       dup_note = note.dup
@@ -177,7 +188,7 @@ RSpec.describe Note do
     it 'generates a new uuid' do
       note = described_class.create!(
         uid: SecureRandom.uuid,
-        user: User.create!(email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
+        user: create_user
       )
 
       dup_note = note.dup
@@ -189,12 +200,12 @@ RSpec.describe Note do
 
   describe '#archive!' do
     it 'sets the archived_at timestamp' do
-      now = Time.local(2016, 8, 1, 15, 33)
+      now = Time.zone.local(2016, 8, 1, 15, 33)
 
       Timecop.freeze(now) do
         note = described_class.create!(
           uid: SecureRandom.uuid,
-          user: User.create!(email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf')
+          user: create_user
         )
 
         note.archive!
@@ -206,80 +217,100 @@ RSpec.describe Note do
 
   describe '.archived' do
     it 'returns only the archived note' do
-      user = User.create!(
-        email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf'
-      )
-      archived = Note.create!(
+      user = create_user
+
+      archived = described_class.create!(
         uid: SecureRandom.uuid,
         user: user,
-        archived_at: Time.local(2016, 8, 1, 15, 33)
+        archived_at: Time.zone.local(2016, 8, 1, 15, 33)
       )
 
-      Note.create!(
+      described_class.create!(
         uid: SecureRandom.uuid,
         user: user
       )
 
-      expect(Note.archived).to eq [archived]
+      expect(described_class.archived).to eq [archived]
     end
   end
 
   describe '.unarchived' do
     it 'returns only the unarchived note' do
-      user = User.create!(
-        email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf'
-      )
-      Note.create!(
+      user = create_user
+
+      described_class.create!(
         uid: SecureRandom.uuid,
         user: user,
-        archived_at: Time.local(2016, 8, 1, 15, 33)
+        archived_at: Time.zone.local(2016, 8, 1, 15, 33)
       )
 
-      unarchived = Note.create!(
+      unarchived = described_class.create!(
         uid: SecureRandom.uuid,
         user: user
       )
 
-      expect(Note.unarchived).to eq [unarchived]
+      expect(described_class.unarchived).to eq [unarchived]
     end
   end
 
   describe 'validation: does_not_exceed_free_count_limit' do
-    context 'pro subscription' do
-      let(:user) { User.create! email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', subscription: :pro }
+    context 'with pro subscription' do
+      let(:user) { create_user(subscription: :pro) }
 
       it 'allows unlimited notes' do
         stub_const('Note::FREE_COUNT_LIMIT', 1)
-        Note.create! title: 'my_note', user: user, uid: SecureRandom.uuid
+        described_class.create!(
+          title: 'my_note', user: user, uid: SecureRandom.uuid
+        )
 
-        note = Note.create title: 'my_note', user: user, uid: SecureRandom.uuid
+        note = described_class.create(
+          title: 'my_note', user: user, uid: SecureRandom.uuid
+        )
 
         expect(note).to be_persisted
       end
 
       it 'does not invoke the callback' do
-        note = Note.new title: 'my_note', user: user, uid: SecureRandom.uuid
-
-        expect(note).not_to receive(:does_not_exceed_free_count_limit)
+        note = described_class.new(
+          title: 'my_note', user: user, uid: SecureRandom.uuid
+        )
+        allow(note).to receive(:does_not_exceed_free_count_limit)
 
         note.save!
+
+        expect(note).not_to have_received(:does_not_exceed_free_count_limit)
       end
     end
 
-    context 'free subscription' do
-      let(:user) { User.create! email: 'user@example.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', subscription: :free }
+    context 'with free subscription' do
+      let(:user) { create_user(subscription: :free) }
 
       it 'fails validation when limit is exceeded' do
         stub_const('Note::FREE_COUNT_LIMIT', 1)
-        Note.create! title: 'my_note', user: user, uid: SecureRandom.uuid
+        described_class.create!(
+          title: 'my_note', user: user, uid: SecureRandom.uuid
+        )
 
-        note = Note.create title: 'my_note', user: user, uid: SecureRandom.uuid
+        note = described_class.create(
+          title: 'my_note', user: user, uid: SecureRandom.uuid
+        )
 
         expect(note).not_to be_persisted
         expect(note.errors[:base]).to eq [
-          'You have reached your note limit of 1 notes. Please delete some notes or upgrade your subscription.'
+          'You have reached your note limit of 1 notes. ' \
+          'Please delete some notes or upgrade your subscription.'
         ]
       end
     end
+  end
+
+  def create_user(attributes = {})
+    attributes = {
+      email: 'user@example.com',
+      password: 'asdfasdf',
+      password_confirmation: 'asdfasdf'
+    }.merge(attributes)
+
+    User.create!(attributes)
   end
 end
