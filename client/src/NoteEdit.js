5d89813d433e4c2bddce0f712ed3ed9c38948d1c
Alexis Reigel <mail@koffeinfrei.org>
2021-01-03 14:49:58 +0100

    remove leftover of direct call to localstorage
    use the abstraction SyncStorage instead

--- client/src/NoteEdit.js
+++ client/src/NoteEdit.js
@@ -12,6 +12,7 @@ import AddNoteButton from './AddNoteButton';
 import AlertFlash from './AlertFlash';
 import EventHive from './EventHive';
 import AutoSave from './AutoSave';
+import SyncStorage from './SyncStorage';
 
 class NoteEdit extends Component {
   constructor(props, context) {
@@ -230,7 +231,7 @@ class NoteEdit extends Component {
       dataType: 'json',
     })
     .done(() => {
-      localStorage.removeItem('note-' + note.uid);
+      SyncStorage.remove(note);
 
       if (this.state.note.uid === note.uid) {
         this.setNewNote();
