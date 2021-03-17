2da46833c8a49fe6983b6cc28ab237e5050212fa
Alexis Reigel <mail@koffeinfrei.org>
2021-03-17 21:55:33 +0100

    split big function `performAjax`

--- client/src/ajax.js
+++ client/src/ajax.js
@@ -18,6 +18,28 @@ export function objectWithNestedKeys(data, nestKey) {
   return newData;
 }
 
+function performAjax(url, method = 'GET', data) {
+  url = setApiHost(url);
+
+  const spinnerId = startSpinner();
+
+  // make request abortable
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const promise = new Promise((resolve, reject) => {
+    let options;
+    ({ url, options } = setUrlandOptions(url, method, data, signal));
+
+    fetch(url, options)
+      .then(response => handleThen(response, resolve, reject))
+      .catch((error) => handleCatch(error, reject))
+      .finally(() => stopSpinner(spinnerId));
+  })
+
+  return { promise, controller };
+}
+
 function setApiHost(url) {
   if (!window.API_HOST) {
     return url;
@@ -26,78 +48,79 @@ function setApiHost(url) {
   return `${window.API_HOST}${url}`;
 }
 
-function performAjax(url, method = 'GET', data) {
-  url = setApiHost(url);
-
+function startSpinner() {
   // start the spinner after 500ms
   const timeoutId = setTimeout(() => {
     EventHive.publish('spinner.toggle', { show: true })
   }, 500)
 
-  // make request abortable
-  const controller = new AbortController();
-  const signal = controller.signal;
+  return timeoutId;
+}
 
-  const promise = new Promise((resolve, reject) => {
-    const options = {
-      method,
-      signal,
-      headers: {
-        'Accept': 'application/json'
-      }
+function stopSpinner(timeoutId) {
+  // stop the spinner
+  clearTimeout(timeoutId);
+  EventHive.publish('spinner.toggle', { show: false })
+}
+
+function setUrlandOptions(url, method, data, signal) {
+  const options = getOptions(method, signal);
+
+  if (data) {
+    // filter out non-values
+    data = Object.fromEntries(
+      Object.entries(data).filter(([_key, value]) => value !== undefined)
+    );
+
+    if (method === 'GET') {
+      const params = new URLSearchParams(data);
+      url = `${url}?${params}`;
     }
-
-    if (data) {
-      // filter out non-values
-      data = Object.fromEntries(
-        Object.entries(data).filter(([_key, value]) => value !== undefined)
-      );
-
-      if (method === 'GET') {
-        const params = new URLSearchParams(data);
-        url = `${url}?${params}`;
-      }
-      else {
-        const formData = new FormData();
-        Object.keys(data).forEach(key => formData.append(key, data[key]));
-        options.body = formData;
-      }
+    else {
+      const formData = new FormData();
+      Object.keys(data).forEach(key => formData.append(key, data[key]));
+      options.body = formData;
     }
+  }
 
-    fetch(url, options)
-      .then(response => {
-        // 204 No Content
-        if (response.ok && response.status === 204) {
-          resolve();
-        }
-        else if (response.ok) {
-          response.json().then(json => resolve(json));
-        }
-        else {
-          response.json().then(json => reject(
-            new Error(response.statusText),
-            response.status,
-            json
-          ));
-        }
-      })
-      .catch((error) => {
-        // ignore when the request was aborted
-        // (this happens usually when not yet finished request is replaced by a
-        // next request)
-        if (error instanceof DOMException && error.name === 'AbortError') {
-          // no-op
-        }
-        else {
-          reject(error);
-        }
-      })
-      .finally(() => {
-        // stop the spinner
-        clearTimeout(timeoutId);
-        EventHive.publish('spinner.toggle', { show: false })
-      });
-  })
+  return { url, options };
+}
 
-  return { promise, controller };
+function getOptions(method, signal) {
+  return {
+    method,
+    signal,
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+}
+
+function handleThen(response, resolve, reject) {
+  // 204 No Content
+  if (response.ok && response.status === 204) {
+    resolve();
+  }
+  else if (response.ok) {
+    response.json().then(json => resolve(json));
+  }
+  else {
+    response.json().then(json => reject(
+      new Error(response.statusText),
+      response.status,
+      json
+    ));
+  }
+}
+
+function handleCatch(error, reject) {
+  // ignore when the request was aborted
+  // (this happens usually when not yet finished request is replaced by a
+  // next request)
+  if (error instanceof DOMException && error.name === 'AbortError') {
+    // no-op
+  }
+  else {
+    reject(error);
+  }
 }
