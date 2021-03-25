b338c3899e8bbebe66dc5cae5c456606acaf1f08
Alexis Reigel <mail@koffeinfrei.org>
2021-03-25 21:51:34 +0100

    only build client for feature specs
    for the rest of the specs we can enjoy the usual quick feedback

--- spec/support/config/public_deploy.rb
+++ spec/support/config/public_deploy.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  # only build the client when we run feature specs
+  break if config.files_to_run.none? { |file| file.include?('features/') }
+
   deploy_client = DeployClient.new
   config.before :suite do
     puts '--> Building the client app and deploying it ' \
