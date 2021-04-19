33c9e25aacde5d82e7683b708ed3b43663801fce
Alexis Reigel <mail@koffeinfrei.org>
2021-04-19 20:49:56 +0200

    operate NoteTaskPopulator on text_content
    the note spec 'duplicates the images' started failing when setting
    `#content`, because nokogiri tried to parse the base64 images.
    while this may only be an issue with the test, it's a good pointer to
    actually only operate on `#text_content` which is the database column.
    `#content` is the attribute that CarrierWave::Base64ImageContent
    operates on. if we modify that field we invoke the image parsing, which
    may be a bit more unnecessary overhead.

--- app/services/note_task_populator.rb
+++ app/services/note_task_populator.rb
@@ -6,14 +6,14 @@ class NoteTaskPopulator
   end
 
   def run
-    html = Nokogiri::HTML::DocumentFragment.parse(@note.content)
+    html = Nokogiri::HTML::DocumentFragment.parse(@note.text_content)
 
     @note.tasks = html.css('ul.task-list li').map do |checkbox|
       build_task!(checkbox)
     end
 
     # save_with -> don't add newlines, it will break quill
-    @note.content = html.to_html(save_with: 0)
+    @note.text_content = html.to_html(save_with: 0)
   end
 
   private
