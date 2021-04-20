c90c7dc5037ba400087eea18d6828f7815b95d4f
Alexis Reigel <mail@koffeinfrei.org>
2021-04-20 20:49:14 +0200

    allow specifying the capybara driver as env var

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -7,11 +7,25 @@ Capybara.register_driver :firefox_headless do |app|
   Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
 end
 
-Capybara.javascript_driver = :firefox_headless
+Capybara.register_driver :firefox do |app|
+  options = ::Selenium::WebDriver::Firefox::Options.new
+
+  Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
+end
+
+Capybara.javascript_driver =
+  if ENV['DRIVER'].present?
+    ENV['DRIVER'].to_sym
+  else
+    :firefox_headless
+  end
 
 Capybara.server = :webrick
 
 # From https://github.com/mattheworiordan/capybara-screenshot/issues/84#issuecomment-41219326
 Capybara::Screenshot.register_driver(:firefox_headless) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
+Capybara::Screenshot.register_driver(:firefox) do |driver, path|
   driver.browser.save_screenshot(path)
 end
