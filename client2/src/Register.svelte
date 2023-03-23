11b6bcff3e71aa368c6cc0ac102408fd0f71073d
Alexis Reigel <mail@koffeinfrei.org>
2023-03-23 20:37:10 +0100

    disable registration

--- client2/src/Register.svelte
+++ client2/src/Register.svelte
@@ -1,84 +1,17 @@
 <UserForm>
-  <form on:submit={handleFormSubmit}>
-    <Utf8 />
-
-    <div class="flex one">
-      <TextInput
-        type="email"
-        model="user"
-        attribute="email"
-        label="Email"
-        on:change={setValue} />
-
-      <TextInput
-        type="password"
-        model="user"
-        attribute="password"
-        label="Password"
-        on:change={setValue} />
-
-      <TextInput
-        type="password"
-        model="user"
-        attribute="password_confirmation"
-        label="Confirm password"
-        on:change={setValue} />
-    </div>
-
-    <SubmitButton label="Register" />
-  </form>
-
   <svelte:fragment slot="intro">
     <p>
-      You can register for a <strong>free account</strong> which <strong>limits</strong> you to <strong>100 notes</strong>.
-      <br/>
-      This free hosting service is sponsored by <a href="https://www.panter.ch">Panter AG</a>.
+      Registration is closed at this moment. I'm hosting this app
+      privately. I currently couldn't comply with the <a href="https://www.kmu.admin.ch/kmu/en/home/facts-and-trends/digitization/data-protection/new-federal-act-on-data-protection-nfadp.html">New Federal Act on Data Protection (nFADP)</a>,
+      which is why I don't want to host any private data of other users.
     </p>
     <p>
-      Alternatively you may <a href="https://github.com/panter/mykonote">get the source code from
+      While I'm figuring out all that legal stuff you may <a href="https://github.com/panter/mykonote">get the source code from
       GitHub</a> and install it on a server on your own.
     </p>
   </svelte:fragment>
 </UserForm>
 
 <script>
-  import { push } from 'svelte-spa-router'
-  import { ajax } from './ajax'
-  import { show, clear } from './flash'
-  import { scrollToTop } from './scroll'
-  import { isAuthenticated } from './stores'
-  import TextInput from './form/TextInput.svelte'
-  import SubmitButton from './form/SubmitButton.svelte'
-  import Utf8 from './form/Utf8.svelte'
-  import Navbar from './Navbar.svelte'
-  import Flash from './Flash.svelte'
   import UserForm from './UserForm.svelte'
-
-  let values
-
-  const setValue = event => {
-    values = { ...values, ...event.detail }
-  }
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault()
-
-    ajax('/users', 'POST', values)
-      .then((data) => {
-        clear('alert')
-        $isAuthenticated = true
-        show('notice',
-          'Great! Glad you made it!<br>' +
-            'You have been subscribed to the <strong>free plan</strong> which ' +
-            '<strong>limits</strong> you to have <strong>100 notes</strong>.'
-        )
-        push('/notes')
-      })
-      .catch((error) => {
-        const errors = error.responseJson.errors.join('<br>')
-
-        show('alert', 'Sorry, that did not work. You need to fix your inputs:<br>' + errors)
-      })
-      .finally(scrollToTop)
-  }
 </script>
