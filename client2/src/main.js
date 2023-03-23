469c91661cccfc0689ba351ae62f984c8f419cae
Alexis Reigel <mail@koffeinfrei.org>
2023-03-23 20:13:12 +0100

    WIP remove sw for now

--- client2/src/main.js
+++ client2/src/main.js
@@ -1,11 +1,8 @@
 import './App.sass'
 import App from './App.svelte'
-import registerServiceWorker from './registerServiceWorker';
 
 const app = new App({
   target: document.getElementById('app')
 })
-
-registerServiceWorker();
 
 export default app
