9bc763bc03fb0ae3e7b5978c17ce0130edb09b10
Alexis Reigel <mail@koffeinfrei.org>
2023-03-24 20:02:41 +0100

    remove obsolete client (react)

--- client/src/feature.js
+++
@@ -1,7 +0,0 @@
-export function isFeatureEnabled(name) {
-  // enable all features for browser tests
-  if (navigator.webdriver) { return true; }
-
-  const item = localStorage.getItem(`feature-${name}`);
-  return !!JSON.parse(item);
-}
