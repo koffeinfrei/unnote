6a7db94a3885675beaedf36c142e8f18a5526732
Alexis Reigel <mail@koffeinfrei.org>
2023-03-25 21:12:11 +0100

    minimal service worker
    This is just to enable PWA. PWA already seems to work, but according to
    https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Installable_PWAs#requirements:
    
    > A service worker registered, to allow the app to work offline (this is
    > required only by Chrome for Android currently)

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,10 @@
 import './App.sass'
 import App from './App.svelte'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+}
+
 const app = new App({
   target: document.getElementById('app')
 })
