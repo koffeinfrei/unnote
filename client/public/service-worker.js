cf62462c698df57645990243ba5def98e2221592
Alexis Reigel <mail@koffeinfrei.org>
2023-03-31 17:52:26 +0200

    share data from mobile share to menu

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -4,9 +4,18 @@ self.addEventListener('fetch', (fetchEvent) => {
       (async () => {
         const formData = await fetchEvent.request.formData();
         const image = formData.get('image');
+        const title = formData.get('title');
+        const text = formData.get('text');
+        const url = formData.get('url');
+
         const keys = await caches.keys();
         const mediaCache = await caches.open(keys.filter((key) => key.startsWith('media'))[0]);
-        await mediaCache.put('shared-image', new Response(image));
+
+        if (image) {
+          await mediaCache.put('shared-image', new Response(image));
+        } else {
+          await mediaCache.put('shared-data', new Response(JSON.stringify({ title, text, url })));
+        }
         return Response.redirect('/#/notes?share-target', 303);
       })(),
     );
