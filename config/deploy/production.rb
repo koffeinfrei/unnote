3eef57ef278eef89b16a652a0d69e75f693a5d00
Alexis Reigel <mail@koffeinfrei.org>
2023-04-01 23:02:25 +0200

    cap task to change the staging app name
    The PWA name for staging needs to be different, such that we can install
    them as 2 different apps.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,6 @@
 server 'mykonote-production', user: 'deploy', roles: %w[web app db]
 
 set :deploy_to, "/home/deploy/#{fetch :application}"
+
+# custom var, used for PWA name
+set :application_name, 'Mykonote'
