1f5ae8c59616033c4f3b94e11e2e2306c6a01b54
Alexis Reigel <mail@koffeinfrei.org>
2023-09-02 09:45:46 +0200

    rename cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_mykonote_session', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_unnote_session', domain: :all, tld_length: 2
