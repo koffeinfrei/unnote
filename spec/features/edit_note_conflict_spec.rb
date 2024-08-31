ecf0bb3445b3c5c6a421ab1197856baf4f9e68ff
Alexis Reigel <mail@koffeinfrei.org>
2024-08-31 16:59:44 +0200

    make the conflict spec less flaky
    The dependence on the time could have made the spec flaky (with
    timecop), as depending on the specs running speed the advancement in
    secods was not deterministic really. We now set explicit times when
    simulating the conflict.

--- spec/features/edit_note_conflict_spec.rb
+++ spec/features/edit_note_conflict_spec.rb
@@ -26,22 +26,24 @@ RSpec.describe 'Edit note with conflict', :js do
       expect(page).to have_content 'note content'
 
       # ...in the meantime there's an edit
+      travel_to Time.zone.local(2016, 8, 1, 15, 34)
       note.update! content: '<p>note content - update 1</p>'
 
+      travel_to Time.zone.local(2016, 8, 1, 15, 35)
       find('.ql-editor').set('note content - update 2')
 
       after_save_cycle do
         expect(page).to have_content 'SAVED'
       end
 
-      expect(page).to have_content 'my note (conflict 2016-08-01 15:33)'
+      expect(page).to have_content 'my note (conflict 2016-08-01 15:35)'
 
       # the note has the updated content
       expect(note.reload.content).to eq '<p>note content - update 2</p>'
 
       # the conflict copy has the old content
       expect(Note.last).to have_attributes(
-        title: 'my note (conflict 2016-08-01 15:33)',
+        title: 'my note (conflict 2016-08-01 15:35)',
         content: '<p>note content - update 1</p>'
       )
     end
